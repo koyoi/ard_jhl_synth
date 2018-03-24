d20493aa843e164bae95e2ce89ddbcf52986b1b6
koyoi <mayura@s8.xrea.com>
2018-03-25 02:10:26 +0900

    エンベローブok

--- calc_osc.hpp
+++ calc_osc.hpp
@@ -4,61 +4,64 @@
 
 #include "Oscil.h"
 
-#define DEF_NUM_TABLE_CELLS	1024
+#define DEF_NUM_TABLE_CELLS	2048
 
 typedef enum _waveForm
 {
     WF_SIN,
     WF_SQUARE,
     WF_SAW,
-    WF_TRIG,
+    WF_TRI,
 }waveForm;
+
+char* wfnames[] = { "sin", "squ", "saw", "tri" };
 
 extern const int8_t SIN2048_DATA[];
 
 typedef int8_t(*wf_func)(unsigned int);
 
-/*
-(int8_t)pgm_read_byte_near(table
-*/
 
-int8_t wf_sin1024(unsigned int pos) {
-	int8_t tmp = (int8_t)pgm_read_byte_near(SIN2048_DATA + pos * 2);
+int8_t wf_sin(unsigned int pos) {
+	int8_t tmp = (int8_t)pgm_read_byte_near(SIN2048_DATA + pos);
 	return( tmp );	// todo pow
 };
 
-int8_t wf_square1024(unsigned int pos) {
+int8_t wf_square(unsigned int pos) {
 	if (pos < DEF_NUM_TABLE_CELLS / 2){
-		return(-128);
+		return(120);
 	}else {
-		return(127);
+		return(0);
 	}
 }
 
-int8_t wf_saw1024(unsigned int pos) {
+int8_t wf_saw(unsigned int pos) {
 	return(0);
 }
 
-int8_t wf_trig1024(unsigned int pos) {
+int8_t wf_tri(unsigned int pos) {
 	return(0);
 }
 
 
 #define NUM_WG	2
-wf_func	wg[NUM_WG] = { wf_sin1024, wf_square1024 };
+wf_func	wg_fn[NUM_WG] = { wf_sin, wf_square };
 int16_t	wg_param[NUM_WG];
 
 
 void setWgWf(uint8_t op, waveForm wf)
 {
 	wf_func	_wf;
+	char buf_printf[16];
+
 	switch (wf) {
-	case WF_SQUARE: _wf = wf_square1024; break;
-	case WF_SAW:	_wf = wf_saw1024; break;
-	case WF_TRIG:	_wf = wf_trig1024; break;
-	default:		_wf = wf_sin1024; break;
+	case WF_SQUARE: _wf = wf_square; break;
+	case WF_SAW:	_wf = wf_saw; break;
+	case WF_TRI:	_wf = wf_tri; break;
+	default:		_wf = wf_sin; break;
 	};
-	wg[op] = _wf;
+	wg_fn[op] = _wf;
+	sprintf(buf_printf, "wg_func[%d]:%s(%d)", op, wfnames[wf], wf);
+	Serial.println(buf_printf);
 }
 
 void setWgPar(uint8_t op, int16_t _par)
@@ -68,30 +71,33 @@ void setWgPar(uint8_t op, int16_t _par)
 
 
 // -------------------------------------------------------------------
+
+typedef enum _gen_type
+{
+    TYPE_WAVETABLE,
+    TYPE_CALC
+}gen_type;
+
 template <uint16_t NUM_TABLE_CELLS, uint16_t UPDATE_RATE>
 class calc_osc : public Oscil <NUM_TABLE_CELLS, UPDATE_RATE >
 {
 private:
-	uint8_t		n_wg;	// ‰½”Ô‚Ì”­MŠí‚ð“Ç‚Þ‚©
-
+	uint8_t		n_wg;	// ï¿½ï¿½ï¿½Ô‚Ì”ï¿½ï¿½Mï¿½ï¿½ï¿½Ç‚Þ‚ï¿½
+    gen_type    type;
+    
 //	unsigned long phase_fractional;
 //	volatile unsigned long phase_increment_fractional;
 
 public:
-	calc_osc(uint8_t _n) : n_wg(_n) {}
-	calc_osc(): n_wg(0) {}	// null ‰ñ”ð
+	calc_osc(uint8_t _n) : n_wg(_n), type(TYPE_CALC) {}
+	calc_osc(): n_wg(0), type(TYPE_CALC) {}	// null ï¿½ï¿½ï¿½
 	~calc_osc() {}
+	/*	é…ã„æ°—ãŒã™ã‚‹
+	int8_t(calc_osc::*phMod)(Q15n16 phmod_proportion);
+	int8_t(calc_osc::*readTable)();
+	int8_t(calc_osc::*atIndex)(unsigned int index);
+	*/
 
-	void setWG(uint8_t _wg) {
-		n_wg = _wg;
-	}
-
-	inline
-	int8_t next()
-	{
-		this->incrementPhase();
-		return readTable();
-	}
 
 	/** Returns the next sample given a phase modulation value.
 	@param phmod_proportion a phase modulation value given as a proportion of the wave. The
@@ -106,27 +112,69 @@ public:
 	inline
 	int8_t phMod(Q15n16 phmod_proportion)
 	{
-		this->incrementPhase();
-		return wg[n_wg](((this->phase_fractional + (phmod_proportion * NUM_TABLE_CELLS)) >> OSCIL_F_BITS) & (NUM_TABLE_CELLS - 1));
+		if (type == TYPE_WAVETABLE)
+		{
+			Oscil<NUM_TABLE_CELLS, UPDATE_RATE >::phMod(phmod_proportion);
+		}
+		else
+		{
+			this->incrementPhase();
+			return wg_fn[n_wg](((this->phase_fractional + (phmod_proportion * NUM_TABLE_CELLS)) >> OSCIL_F_BITS) & (NUM_TABLE_CELLS - 1));
+		}
 	}
 
 	/** Returns the current sample.
 	*/
 	inline
 	int8_t readTable()
-	{
+	{        
+		if(type==TYPE_WAVETABLE)
+        {
+            Oscil<NUM_TABLE_CELLS, UPDATE_RATE >::readTable();
+        }
+        else
+        {
+			setPin13High();
+
 #ifdef OSCIL_DITHER_PHASE
-		return wg[n_wg](((phase_fractional + ((int)(xorshift96() >> 16))) >> OSCIL_F_BITS) & (NUM_TABLE_CELLS - 1));
+		return wg_fn[n_wg](((phase_fractional + ((int)(xorshift96() >> 16))) >> OSCIL_F_BITS) & (NUM_TABLE_CELLS - 1));
 #else
-		return wg[n_wg]((this->phase_fractional >> OSCIL_F_BITS) & (NUM_TABLE_CELLS - 1));
+		return wg_fn[n_wg]((this->phase_fractional >> OSCIL_F_BITS) & (NUM_TABLE_CELLS - 1));
 #endif
-	}
+		  setPin13Low();
+
+	    }
+    }
 
 	inline
 	int8_t atIndex(unsigned int index)
 	{
-		return wg[n_wg](index & (NUM_TABLE_CELLS - 1));
+        if(type==TYPE_WAVETABLE)
+        {
+            Oscil<NUM_TABLE_CELLS, UPDATE_RATE >::atIndex(index);
+        }
+        else
+        {
+            return wg_fn[n_wg](index & (NUM_TABLE_CELLS - 1));
+        }
 	}
+
+	void setTable(const int8_t * TABLE_NAME)
+	{
+		char buf_printf[32];
+
+		type = TYPE_WAVETABLE;
+		this->table = TABLE_NAME;
+//		phMod = &Oscil<NUM_TABLE_CELLS, UPDATE_RATE >::phMod;
+		sprintf(buf_printf, "opr[%d]:table(%d)", this, TABLE_NAME);
+		Serial.println(buf_printf);
+	}
+
+	void setWG(uint8_t _wg) {
+		type = TYPE_CALC;
+		n_wg = _wg;
+	}
+
 };
 
 #undef NUM_TABLE_CELLS
--- jhl_synth.ino
+++ jhl_synth.ino
@@ -19,7 +19,7 @@
 
 	
 	*/
-
+#include <MIDI.h>
 
 #include <MozziGuts.h>
 #include <Mozzi_Utils.h>
@@ -28,7 +28,7 @@
 #include <AudioDelayFeedback.h>
 #include <LowPassFilter.h>
 #include <mozzi_midi.h>
-#include <PDResonant.h>
+//#include <PDResonant.h>
 #include <Phasor.h>
 #include <ReverbTank.h>
 //#include <twi_nonblock.h>
@@ -44,25 +44,18 @@
 
 #include "calc_osc.hpp"
 
+
+
 // use #define for CONTROL_RATE, not a constant
 #define CONTROL_RATE 64 // powers of 2 please
 
-#define NUM_OPR 4
-#define NUM_OPR_CALC	4
+#define NUM_OPR 6
 
 // use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
-Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> opr[NUM_OPR];
-ADSR  <CONTROL_RATE, CONTROL_RATE > adsr[NUM_OPR];
+calc_osc <2048, AUDIO_RATE> opr[NUM_OPR];
+ADSR	<CONTROL_RATE, CONTROL_RATE > adsr[NUM_OPR];
 int adsr_cache[NUM_OPR];
 bool opr_on_flags[NUM_OPR];
-
-
-
-calc_osc <1024, AUDIO_RATE> opr_calc[NUM_OPR_CALC];
-ADSR	<CONTROL_RATE, CONTROL_RATE > adsr_calc[NUM_OPR_CALC];
-int adsr_calc_cache[NUM_OPR_CALC];
-bool opr_calc_on_flags[NUM_OPR_CALC];
-
 
 
 #define ADFB_LEN	64
@@ -76,74 +69,129 @@ const int8_t* waveTables[] = {SIN2048_DATA, SAW2048_DATA,
                        };
 
 
+void HandleNoteOn(byte channel, byte note, byte velocity) { 
+//  aSin.setFreq(mtof(float(note)));
+//  envelope.noteOn();
+//  digitalWrite(LED,HIGH);
+}
+
+void HandleNoteOff(byte channel, byte note, byte velocity) { 
+//  envelope.noteOff();
+//  digitalWrite(LED,LOW);
+}
+
+void h_cc(byte channel , byte number , byte value )
+{
+
+}
+
+void h_pc(byte channel , byte number )
+{
+
+
+}
+
+
+//#define SERIAL_IS_MIDI
 void setup(){
+#ifdef SERIAL_IS_MIDI
+  MIDI_CREATE_DEFAULT_INSTANCE();
+
+  // Connect the HandleNoteOn function to the library, so it is called upon reception of a NoteOn.
+  MIDI.setHandleNoteOn(HandleNoteOn);  // Put only the name of the function
+  MIDI.setHandleNoteOff(HandleNoteOff);  // Put only the name of the function
+
+  MIDI.setHandleControlChange( h_cc );
+  MIDI.setHandleProgramChange( h_pc );
+//void setHandlePitchBend( funcname )
+//void setHandleSystemExclusive( funcname )
+
+  // Initiate MIDI communications, listen to all channels (not needed with Teensy usbMIDI)
+  MIDI.begin(MIDI_CHANNEL_OMNI);  
+    
+//  envelope.setADLevels(255,64);
+//  envelope.setTimes(50,200,10000,200); // 10000 is so the note will sustain 10 seconds unless a noteOff comes
+
+#else
   Serial.begin(115200);
   Serial.println( "start" );
+#endif
 
   startMozzi(CONTROL_RATE); // set a control rate of 64(Hz) (powers of 2 please)
 
   setPin13Out();  // 負荷モニタ
   
   // dummy
+
+  setWgWf(0, WF_SIN);
+  setWgWf(1, WF_SQUARE);
+
   opr[0].setTable(SIN2048_DATA);
   opr[1].setTable(SAW2048_DATA);
   opr[2].setTable(TRIANGLE_VALVE_2048_DATA);
   opr[3].setTable(TRIANGLE_HERMES_2048_DATA);
-  opr[0].setFreq(440);
-  opr[1].setFreq(440);
-  opr[2].setFreq(440);
-  opr[3].setFreq(440);
+  opr[4].setWG(0);
+  opr[5].setWG(1);
 
-  opr_calc[0].setFreq(500);
-  opr_calc[1].setFreq(500);
-  opr_calc[1].setWG(1);
+  for( int i = 0; i<NUM_OPR; i++ )
+  {
+    opr[i].setFreq(440/*+31*i*/);
+  } 
 }
+
+
+void print_note_on_monitor()
+{
+	// フラグ状態の表示
+	uint16_t flags = 0;
+	for (int i = 0; i < NUM_OPR; i++)
+	{
+		flags <<= 1;
+		if (opr_on_flags[i]) { flags |= 1; }
+	}
+	flags |= 1 << 15;	// ゼロサプレス簡易回避
+	Serial.println(flags, BIN);
+
+}
+
 
 
 void updateControl(){
   // put changing controls in here
 //  setPin13High();
 
-  for (int i = 0; i<NUM_OPR; i++)
-  {
-	  if (opr_on_flags[i]) {
-		  adsr[i].update();
-		  adsr_cache[i] = adsr[i].next();
-	  }
-  }
-
-  for (int i = 0; i<NUM_OPR_CALC; i++)
-  {
-	  if (opr_calc_on_flags[i]) {
-		  adsr_calc[i].update();
-		  adsr_calc_cache[i] = adsr_calc[i].next();
-	  }
-  }
+	for (int i = 0; i < NUM_OPR; i++)
+	{
+		if (opr_on_flags[i]) 
+		{
+			adsr[i].update();
+			adsr_cache[i] = adsr[i].next();
+			if (!adsr[i].playing())
+			{
+				opr_on_flags[i] = 0;
+				print_note_on_monitor();
+			}
+		}
+	}
 //  setPin13Low();
 }
 
 
 // AUDIO_RATE で呼ばれる
 int updateAudio(){
-  setPin13High();
+//  setPin13High();
 
   int16_t t = 0;
   int16_t wet = 0;
 
   for(int i=0; i<NUM_OPR; i++ )
   {
-    if( opr_on_flags[i] ){
-		t += opr[i].next();// *adsr_cache[i];
-//      t += pow( t, 1.5f );
-    }
-  }
-
-  for (int i = 0; i<NUM_OPR_CALC; i++)
-  {
-	  if (opr_calc_on_flags[i]) {
-		  t += opr_calc[i].next();// *adsr_calc_cache[i];
+	  if (opr_on_flags[i])
+	  {
+		  t += (opr[i].next() * adsr_cache[i])>>8;
 	  }
   }
+
   wet = t;
   //wet = chorus.next( (int8_t)(t/8), (uint16_t)chorus_time );
   setPin13Low();
@@ -152,38 +200,63 @@ int updateAudio(){
 }
 
 
-
 void loop(){
   char cmd;
+  char cmd_i;
+  static uint8_t last_ch;
+
   audioHook(); // required here
 
   // シリアルからテスト発音
   if (Serial.available() > 0) {
     cmd = Serial.read();
-	if ('0' <= cmd ){
-		if( cmd <= '3'){
-			// フラグの反転
-			opr_on_flags[cmd - '0'] = !opr_on_flags[cmd - '0'];
-		}
-		else if (cmd <= '7') {
-			opr_calc_on_flags[cmd - '0'-4] = !opr_calc_on_flags[cmd - '0'-4];
-		}
+	cmd_i = cmd - '0';
 
-      // フラグ状態の表示
-      uint16_t flags = 0;
-	  for (int i = 0; i<NUM_OPR; i++)
-	  {
-		  flags <<= 1;
-		  if (opr_on_flags[i]) { flags |= 1; }
-	  }
-	  for (int i = 0; i<NUM_OPR_CALC; i++)
-	  {
-		  flags <<= 1;
-		  if (opr_calc_on_flags[i]) { flags |= 1; }
-	  }
-	  flags |= 1<<15;	// ゼロサプレス簡易回避
-      Serial.println(flags,BIN );
-    }
+    if (0 <= cmd_i && cmd_i <= NUM_OPR ){
+		last_ch = cmd_i;
+        // フラグの反転
+		opr_on_flags[cmd_i] ^= 1;
+		if (opr_on_flags[cmd_i])
+		{
+			adsr[cmd_i].noteOn();
+			opr_on_flags[cmd_i] = 1;
+		}
+		else
+		{
+			adsr[cmd_i].noteOff();
+		}
+	}
+
+	if (cmd == 'x')
+	{
+		uint16_t temp[6];
+		char str_buff[32];
+
+		temp[0] = random(120, 250);
+		temp[1] = random(0, temp[0]);
+		adsr[last_ch].setADLevels(temp[0], temp[1]);
+		temp[2] = random(1000);
+		temp[3] = random(1000);
+		temp[4] = random(1000);
+		temp[5] = random(1000);
+		adsr[last_ch].setTimes(temp[2], temp[3], temp[4], temp[5]);
+
+		uint8_t midi_note = random(48, 83);
+		int freq = mtof(midi_note);
+		opr[last_ch].setFreq(freq);
+
+		sprintf(str_buff, "ch[%d]:%d(%dHz) %d,%d %d,%d,%d,%d", last_ch, midi_note, freq,
+			temp[0], temp[1], temp[2], temp[3], temp[4], temp[5]);
+		Serial.println(str_buff);
+	}
+
+	if (cmd == '\n')
+	{
+		print_note_on_monitor();
+	}
   }
+
+
+
 }
 
